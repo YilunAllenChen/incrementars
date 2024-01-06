5fd80db86d50e56bcd410d63f971c85e12d7ff23
Allen Chen <alchen@drwholdings.com>
2024-01-06 12:42:18 -0600

    kind of working

--- src/lib.rs
+++ src/lib.rs
@@ -1,123 +1,149 @@
-use std::collections::HashMap;
+use std::rc::Rc;
 
-type NodeFunc = fn(f32) -> f32;
-type NodeId = usize;
+pub type NodeId = usize;
 
-pub struct Node {
-    pub id: NodeId,
-    pub val: f32,
-    pub dirty: bool,
-    pub children: Vec<NodeId>,
-    pub parents: Vec<NodeId>,
-    pub func: NodeFunc,
+pub trait Node<T> {
+    fn value(&self) -> T;
+    fn stablize(&mut self);
 }
 
-impl Node {
-    pub fn new(id: NodeId, func: NodeFunc) -> Self {
-        Node {
-            id,
-            val: 0.0,
-            dirty: false,
-            children: vec![],
-            parents: vec![],
+#[derive(Copy, Clone)]
+pub struct Var<T>
+where
+    T: Copy + Clone,
+{
+    pub value: T,
+}
+
+impl<T> Node<T> for Var<T>
+where
+    T: Copy,
+{
+    fn stablize(&mut self) {}
+    fn value(&self) -> T {
+        self.value
+    }
+}
+
+pub struct Map2<T, U, V>
+where
+    T: Clone,
+    U: Clone,
+    V: Clone,
+{
+    pub value: T,
+    pub parents: (Rc<dyn Node<U>>, Rc<dyn Node<V>>),
+    pub func: fn(U, V) -> T,
+}
+
+impl<T, U, V> Node<T> for Map2<T, U, V>
+where
+    T: Clone,
+    U: Clone,
+    V: Clone,
+{
+    fn stablize(&mut self) {
+        self.value = (self.func)(self.parents.0.value(), self.parents.1.value());
+    }
+    fn value(&self) -> T {
+        self.value.clone()
+    }
+}
+
+impl<T, U, V> Map2<T, U, V>
+where
+    T: Clone,
+    U: Clone,
+    V: Clone,
+{
+    pub fn from(
+        n1: Rc<impl Node<U> + 'static>,
+        n2: Rc<impl Node<V> + 'static>,
+        func: fn(U, V) -> T,
+    ) -> Self {
+        Map2 {
+            value: (func)(n1.value(), n2.value()),
+            parents: (n1, n2),
             func,
         }
     }
 }
 
-pub fn link(parent: &mut Node, child: &mut Node) {
-    parent.children.push(child.id);
-    child.parents.push(parent.id);
+pub struct Map3<T, U, V, W>
+where
+    T: Clone,
+    U: Clone,
+    V: Clone,
+    W: Clone,
+{
+    pub value: T,
+    pub parents: (Rc<dyn Node<U>>, Rc<dyn Node<V>>, Rc<dyn Node<W>>),
+    pub func: fn(U, V, W) -> T,
 }
 
-pub struct Graph {
-    pub nodes: HashMap<NodeId, Node>,
-}
-
-impl Graph {
-    pub fn set(&mut self, node_id: NodeId, val: f32) {
-        let node = self.nodes.get_mut(&node_id).unwrap();
-        node.val = val;
-
-        let mut stack = node.children.clone();
-        while let Some(node_id) = stack.pop() {
-            let dnode = self.nodes.get_mut(&node_id).unwrap();
-            dnode.dirty = true;
-            stack.extend(dnode.children.clone())
-        }
+impl<T, U, V, W> Node<T> for Map3<T, U, V, W>
+where
+    T: Clone,
+    U: Clone,
+    V: Clone,
+    W: Clone,
+{
+    fn stablize(&mut self) {
+        self.value = (self.func)(
+            self.parents.0.value(),
+            self.parents.1.value(),
+            self.parents.2.value(),
+        );
     }
+    fn value(&self) -> T {
+        self.value.clone()
+    }
+}
 
-    pub fn stabilize(&mut self) {
-        let nodes = &mut self.nodes;
-        let mut stack: Vec<NodeId> = nodes
-            .iter()
-            .filter_map(|(id, node)| if node.dirty { Some(id.clone()) } else { None })
-            .collect();
-
-        let mut new_vals: HashMap<NodeId, f32> = HashMap::new();
-        while let Some(node_id) = stack.pop() {
-            let num_dirty_parents =
-                nodes
-                    .get(&node_id)
-                    .unwrap()
-                    .parents
-                    .iter()
-                    .fold(0, |acc, pa_id| match nodes.get(pa_id).unwrap().dirty {
-                        true => acc + 1,
-                        false => acc,
-                    });
-
-            if num_dirty_parents > 0 {
-                stack.push(node_id);
-            } else {
-                let curr = nodes.get(&node_id).unwrap();
-                let pa_id = curr.parents[0];
-                let pa_val = nodes.get(&pa_id).unwrap().val;
-                new_vals.insert(node_id, (curr.func)(pa_val));
-            }
+impl<T, U, V, W> Map3<T, U, V, W>
+where
+    T: Clone,
+    U: Clone,
+    V: Clone,
+    W: Clone,
+{
+    pub fn from(
+        n1: Rc<impl Node<U> + 'static>,
+        n2: Rc<impl Node<V> + 'static>,
+        n3: Rc<impl Node<W> + 'static>,
+        func: fn(U, V, W) -> T,
+    ) -> Self {
+        Map3 {
+            value: (func)(n1.value(), n2.value(), n3.value()),
+            parents: (n1, n2, n3),
+            func,
         }
-
-        new_vals.into_iter().for_each(|(node_id, new_val)| {
-            let curr = nodes.get_mut(&node_id).unwrap();
-            println!("setting {}: {} -> {}", node_id, curr.val, new_val);
-            curr.val = new_val;
-        })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
-
     #[test]
-    fn test_add() {
-        let mut node0 = Node::new(0, |x| x * 2.0);
-        let mut node1 = Node::new(1, |x| x * 4.0);
-
-        link(&mut node0, &mut node1);
-
-        let mut g = Graph {
-            nodes: HashMap::from_iter(vec![(0, node0), (1, node1)]),
-        };
-
-        g.set(0, 2.0);
-
-        assert_eq!(g.nodes.get(&1).unwrap().dirty, true)
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
     }
 
     #[test]
-    fn test_propagate() {
-        let mut node0 = Node::new(0, |x| x * 2.0);
-        let mut node1 = Node::new(1, |x| x * 4.0);
+    fn test_stablize() {
+        let mut v1 = Rc::new(Var { value: 1 });
+        let v2 = Rc::new(Var { value: 5 });
+        let m1 = Rc::new(Map2::from(v1.clone(), v2, |a, b| a * b));
+        let v3 = Rc::new(Var { value: 2 });
+        let v4 = Rc::new(Var { value: 10 });
 
-        link(&mut node0, &mut node1);
-        let mut g = Graph {
-            nodes: HashMap::from_iter(vec![(0, node0), (1, node1)]),
-        };
+        let mut map3 = Map3::from(m1, v3, v4, |a, b, c| a + b + c);
+        map3.stablize();
 
-        g.set(0, 2.0);
-        g.stabilize();
-        assert_eq!(g.nodes.get(&1).unwrap().val, 8.0)
+        assert_eq!(map3.value, 17);
+
+        v1.value = 3;
+        map3.stablize();
+        assert_eq!(map3.value, 27);
     }
 }
