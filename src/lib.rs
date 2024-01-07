2be3de498f7fb683008744638bc21daa2f76c474
Allen Chen <alchen@drwholdings.com>
2024-01-07 15:18:09 -0600

    /feature bind

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::collections::VecDeque;
 use std::{cell::RefCell, rc::Rc};
 mod node;
 
+use node::bind2::{Bind2, BindHandle};
 use node::map::{Map, MapHandle};
 use node::map2::{Map2, Map2Handle};
 pub use node::traits::{handles, NodeBehavior, NodeId, NodeInput, NodeValue};
@@ -18,6 +19,9 @@ pub struct Graph {
 }
 
 impl Graph {
+    /**
+    Create a new graph
+    */
     pub fn new() -> Self {
         Graph {
             nodes: vec![],
@@ -26,6 +30,10 @@ impl Graph {
         }
     }
 
+    /**
+    Get the children of a node in their behavioral form, agnostic to the kind of data they hold.
+    Used internally by the graph to propagate dirty changes.
+    */
     fn get_children(&self, id: NodeId) -> Vec<NodeBehaviorHandle> {
         match self.node_to_children_id.get(id as usize) {
             Some(children) => children
@@ -36,12 +44,18 @@ impl Graph {
         }
     }
 
-    pub fn gen_id(&mut self) -> NodeId {
+    /**
+    Create unique node id for each new node registered under the graph
+    */
+    fn gen_id(&mut self) -> NodeId {
         let num = self.id_counter;
         self.id_counter += 1;
         num
     }
 
+    /**
+     * Create a variable node, and return its setter and reader handles
+     */
     pub fn var<T: Clone + 'static>(&mut self, value: T) -> (VarHandle<T>, NodeInputHandle<T>) {
         let node_id = self.gen_id();
         let node = Var::make(node_id, value);
@@ -51,6 +65,9 @@ impl Graph {
         (ident, input)
     }
 
+    /**
+     * Create a map node (fn 'In -> 'SelfT), and return its setter and reader handles
+     */
     pub fn map<In, SelfT>(
         &mut self,
         n: &NodeInputHandle<In>,
@@ -72,6 +89,9 @@ impl Graph {
         (ident, input)
     }
 
+    /**
+     * Create a map2 node (fn 'In -> 'In2 -> 'SelfT), and return its setter and reader handles
+     */
     pub fn map2<In1, In2, SelfT>(
         &mut self,
         n1: &NodeInputHandle<In1>,
@@ -99,6 +119,45 @@ impl Graph {
         (ident, input)
     }
 
+    /**
+     * Create a bind2 node (fn 'In -> '[Node<'SelfT>], -> 'Node<'SelfT>), and return its setter and reader handles
+     * This is useful when one wants to create dynamism in node topology. In a complex graph where you want to avoid
+     * refiring graph when there is a change to (temporarily) unused state, you can use bind to create a node that
+     * dynamically choose which node to listen to depending on the input.
+     */
+    pub fn bind2<In, SelfT>(
+        &mut self,
+        n: &NodeInputHandle<In>,
+        func: fn(In, (NodeInputHandle<SelfT>, NodeInputHandle<SelfT>)) -> NodeInputHandle<SelfT>,
+        captured_inputs: (NodeInputHandle<SelfT>, NodeInputHandle<SelfT>),
+    ) -> (BindHandle<SelfT, In>, NodeInputHandle<SelfT>)
+    where
+        In: Clone + 'static,
+        SelfT: Clone + 'static,
+    {
+        let node_id = self.gen_id();
+        let node = Bind2::make(node_id, n, func, captured_inputs.clone());
+        let (ident, input, behavior) = handles(node);
+        self.nodes.push(behavior);
+        self.node_to_children_id.push(vec![]);
+        self.node_to_children_id
+            .get_mut(n.borrow().id() as usize)
+            .unwrap()
+            .push(node_id);
+        [&captured_inputs.0, &captured_inputs.1]
+            .into_iter()
+            .for_each(|i| {
+                self.node_to_children_id
+                    .get_mut(i.borrow().id() as usize)
+                    .unwrap()
+                    .push(node_id);
+            });
+        (ident, input)
+    }
+
+    /**
+    Recompute all the necessary nodes in the graph.
+    */
     pub fn stablize(&mut self) {
         let mut queue = VecDeque::new();
         self.nodes
@@ -215,5 +274,58 @@ mod tests {
         g.stablize();
         assert_eq!(o1.borrow().value(), "13".to_string());
         assert_eq!(o2.borrow().value(), "17".to_string());
+    }
+
+    #[test]
+    fn test_bind() {
+        //      v2                       v2
+        //      |                        |
+        // v1   m1                  v1   m1
+        //   \                           /
+        //    b1 <- c1   becomes       b1 <- c1
+        //    |                        |
+        //    o                        o
+
+        let mut g = Graph::new();
+        let (v1w, v1r) = g.var(1);
+        let (v2w, v2r) = g.var(2);
+        let (_, m1r) = g.map(&v2r, |a| a + 1);
+
+        #[derive(Clone)]
+        enum Ctrl {
+            V1,
+            M1,
+        }
+
+        let (c1w, c1r) = g.var(Ctrl::V1);
+
+        let (_, b1r) = g.bind2(
+            &c1r,
+            move |c, vec| match c {
+                Ctrl::V1 => vec.0,
+                Ctrl::M1 => vec.1,
+            },
+            (v1r, m1r),
+        );
+
+        let (_, o) = g.map(&b1r, |a| a.to_string());
+
+        g.stablize();
+        assert_eq!(o.borrow().value(), "1".to_string());
+
+        c1w.borrow_mut().set(Ctrl::M1);
+        g.stablize();
+        assert_eq!(o.borrow().value(), "3".to_string());
+
+        // a change on v2 should propagte all the way.
+        v2w.borrow_mut().set(5);
+        g.stablize();
+        assert_eq!(o.borrow().value(), "6".to_string());
+
+        // a change on v1 should not propagate because nothing is bound to it.
+        // b1 still points to m1.
+        v1w.borrow_mut().set(10);
+        g.stablize();
+        assert_eq!(o.borrow().value(), "6".to_string());
     }
 }
--- src/node/mod.rs
+++ src/node/mod.rs
@@ -2,3 +2,4 @@ pub mod traits;
 pub mod var;
 pub mod map;
 pub mod map2;
+pub mod bind2;
