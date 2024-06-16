91c306278cfa7c21790bb07740365a7b74ae6882
Allen Chen <alchen@drwholdings.com>
2024-06-16 08:35:36 -0500

    /v3 draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
-mod node;
+pub mod prelude {}
 
-pub mod prelude {
-    pub use crate::node::*;
+type NodeID = usize;
+
+pub trait Node {
+    fn id(&self);
+    fn depth(&self);
+}
+
+pub trait Observable<T> {
+    fn id(&self);
+    fn observe(&self) -> T;
+}
+
+pub struct Var<T> {
+    id: usize,
+    value: T,
+}
+
+pub struct Map<'a, I: 'a, O> {
+    id: usize,
+    fun: &'a dyn Fn(I) -> O,
 }
