c55a37c5447759c7803ccc87f759493c93b799b2
YilunAllenChen <allenchenyilun1999@gmail.com>
2024-06-16 14:16:51 -0500

    /format

--- benches/ben/main.rs
+++ benches/ben/main.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use incrementars::{
     as_input,
     prelude::{Incrementars, Map1},
@@ -24,20 +24,19 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     let mut group = c.benchmark_group("linear with params");
     for input in [100, 1_000, 10_000, 100_000] {
-        group.bench_with_input(BenchmarkId::from_parameter(input), &input, |b, &i|{
+        group.bench_with_input(BenchmarkId::from_parameter(input), &input, |b, &i| {
             b.iter(move || {
-            let count = i;
-            let mut dag = Incrementars::new();
-            let var = dag.var(0);
-            let mut map: Map1<i32, i32> = dag.map(as_input!(var), |x| x + 1);
+                let count = i;
+                let mut dag = Incrementars::new();
+                let var = dag.var(0);
+                let mut map: Map1<i32, i32> = dag.map(as_input!(var), |x| x + 1);
 
-            for _ in 0..count {
-                map = dag.map(as_input!(map), |x| x + 1);
-            }
-            let start = Instant::now();
-            dag.stablize();
-            black_box(start.elapsed().as_nanos());
-
+                for _ in 0..count {
+                    map = dag.map(as_input!(map), |x| x + 1);
+                }
+                let start = Instant::now();
+                dag.stablize();
+                black_box(start.elapsed().as_nanos());
             });
         });
     }
