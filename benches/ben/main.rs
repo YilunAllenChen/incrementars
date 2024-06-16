ba4ae188f244e660feb6350969c556bebc1d8748
YilunAllenChen <allenchenyilun1999@gmail.com>
2024-06-16 14:16:24 -0500

    /more benches

--- benches/ben/main.rs
+++ benches/ben/main.rs
@@ -1,30 +1,46 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
 use incrementars::{
     as_input,
     prelude::{Incrementars, Map1},
 };
 use std::time::Instant;
 
-fn your_function_to_benchmark(count: i32) {
-    let mut dag = Incrementars::new();
-    let var = dag.var(0);
-    let mut map: Map1<i32, i32> = dag.map(as_input!(var), |x| x + 1);
-
-    for _ in 0..count {
-        map = dag.map(as_input!(map), |x| x + 1);
-    }
-    var.set(10);
-    dag.stablize();
-}
-
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("linear", |b| {
-        b.iter(|| {
+        b.iter(move || {
+            let count = 100_000;
+            let mut dag = Incrementars::new();
+            let var = dag.var(0);
+            let mut map: Map1<i32, i32> = dag.map(as_input!(var), |x| x + 1);
+
+            for _ in 0..count {
+                map = dag.map(as_input!(map), |x| x + 1);
+            }
             let start = Instant::now();
-            your_function_to_benchmark(10000);
+            dag.stablize();
             black_box(start.elapsed().as_nanos());
         })
     });
+
+    let mut group = c.benchmark_group("linear with params");
+    for input in [100, 1_000, 10_000, 100_000] {
+        group.bench_with_input(BenchmarkId::from_parameter(input), &input, |b, &i|{
+            b.iter(move || {
+            let count = i;
+            let mut dag = Incrementars::new();
+            let var = dag.var(0);
+            let mut map: Map1<i32, i32> = dag.map(as_input!(var), |x| x + 1);
+
+            for _ in 0..count {
+                map = dag.map(as_input!(map), |x| x + 1);
+            }
+            let start = Instant::now();
+            dag.stablize();
+            black_box(start.elapsed().as_nanos());
+
+            });
+        });
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
